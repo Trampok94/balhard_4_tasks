d1b7a69ee01abd87e9bec49988821f54b411da8d
trampok <trampo95@gmail.com>
2021-06-30 15:28:34 +0300

    Tasks_dict

--- tasks/dict_tasks/unite_dict.py
+++ tasks/dict_tasks/unite_dict.py
@@ -7,9 +7,8 @@ from copy import deepcopy
 
 
 def unite_dict(dict_1: dict, dict_2: dict) -> dict:
-    dict_1_copy = deepcopy(dict_1)
-    # TODO dict_1_copy обновить элементами dict_2
-    return dict_1_copy
+    dict_1.update(dict_2)
+    return dict_1
 
 
 if __name__ == '__main__':
